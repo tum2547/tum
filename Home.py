a56f3d37afbb9523d715c0c99c885dfb3b727e5b
tum2547 <156771696+tum2547@users.noreply.github.com>
2024-02-13 15:25:16 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -12,7 +12,7 @@ def load_lottieurl(url: str):
         return None
     return r.json()
 
-lottie_url_hello = "https://lottie.host/3f647b41-61bf-4d39-93c3-0433420604cc/8NtmEbAWmO.json"
+lottie_url_hello = "https://lottie.host/8f84a42d-4d4e-4f2e-b257-45e13df26e6f/naQnRt27QN.json"
 lottie_url_download = "https://lottie.host/290016a6-d650-4a32-85c7-4d77b0a892ca/4rNL8XZeZt.json"
 lottie_hello = load_lottieurl(lottie_url_hello)
 lottie_download = load_lottieurl(lottie_url_download)

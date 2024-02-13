1b3f31913d362323c74a4430515de576ec9e9c80
tum2547 <156771696+tum2547@users.noreply.github.com>
2024-02-13 15:53:32 +0700

    Update 1_🍀🍀page1.py

--- pages/1_🍀🍀page1.py
+++ pages/1_🍀🍀page1.py
@@ -4,12 +4,23 @@ import requests
 import streamlit as st
 
 st.set_page_config(
-    page_title="Kairung Datascience Project",
+    page_title="Thaanyasit dejsitthawin Project",
     page_icon= ":bar_chart:",
 )
 st.sidebar.success("เลือกรายการด้านบน.")
 
-st.write("# 🍀🍀🍀 การพยากรณ์ราคาหุ้น! 👋  🧑🏽‍🏫 ")
-st.write("### 1.หลักการและเหตุผล")
-st.write("### 2.วัตถุประสงค์")
+st.header("# 🍀🍀🍀 การพยากรณ์ราคาหุ้น! 👋  🧑🏽‍🏫 ")
+st.subheader("### 1.หลักการและเหตุผล")
+st.info("""
+        xxxxxxxxxxxxxxxxxxxxxx
+        xxxxxxxxxxxxxxxxxxxxxx
+        xxxxxxxxxxxxxxxxxxxxxx
+        xxxxxxxxxxxxxxxxxxxxxx
+        """)
+st.write("### 2.วัตถุประสงค์")("""
+        xxxxxxxxxxxxxxxxxxxxxx
+        xxxxxxxxxxxxxxxxxxxxxx
+        xxxxxxxxxxxxxxxxxxxxxx
+        xxxxxxxxxxxxxxxxxxxxxx
+        """)
 st.balloons()
